6321cd6e1df11761bcfb01e70c68ed19641442c0
Sjw <210340027@cauc.edu.cn>
2026-08-03 01:44:27 +0800

    test: define standalone QuoteWords identity

---
+++ test/project_identity_test.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('standalone app uses the QuoteWords product identity', () {
+    final pubspec = File('pubspec.yaml').readAsStringSync();
+    final androidBuild = File(
+      'android/app/build.gradle.kts',
+    ).readAsStringSync();
+    final androidManifest = File(
+      'android/app/src/main/AndroidManifest.xml',
+    ).readAsStringSync();
+    final mainActivity = File(
+      'android/app/src/main/kotlin/tech/undef/quotewords/MainActivity.kt',
+    );
+
+    expect(pubspec, contains('name: quotewords'));
+    expect(pubspec, contains('description: QuoteWords'));
+    expect(androidBuild, contains('namespace = "tech.undef.quotewords"'));
+    expect(androidBuild, contains('applicationId = "tech.undef.quotewords"'));
+    expect(androidManifest, contains('android:label="QuoteWords"'));
+    expect(mainActivity.existsSync(), isTrue);
+    expect(
+      mainActivity.readAsStringSync(),
+      contains('package tech.undef.quotewords'),
+    );
+  });
+}
